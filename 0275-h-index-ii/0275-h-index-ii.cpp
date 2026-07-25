e4e09981f115a808cbca11d76b53a274bd39d078
Vikas Pandey <vikaspandey131118@gmail.com>
2026-07-25 19:45:26 +0530

    Time: 0 ms (100%), Space: 22.6 MB (47.16%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        if(n==1&&citations[0]!=0){
+            return 1;
+        }
+        for(int i =0; i<n; i++){
+            int prices = n-i;
+            if(citations[i]>=prices){
+                return prices;
+            }
+        }
+        return 0;
+    }
+};
