3825dedc6476a14b81c6a2d0be8de9b80fc24b5d
Vikas Pandey <vikaspandey131118@gmail.com>
2026-08-05 23:49:26 +0530

    Time: 171 ms (57.41%), Space: 12.6 MB (91.75%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -8,7 +8,7 @@ public:
        vector<bool>prime(n,true);
        prime[0]=false;
        prime[1]=false;
-       for(int i=2; i*i<=n; i++){
+       for(int i=2; i*i<n; i++){
         if(prime[i]==true){
             for(int j = i*i; j<n;j+=i){
                 prime[j]=false;
